8069f3f79ff3ba031eccbf3c4c31371f2f25be8d
kne2317 <skdms2317@gmail.com>
2021-06-18 04:16:42 +0900

    login

--- main.php
+++ main.php
@@ -17,10 +17,22 @@
      		<h3>다모임</h3>
      		<p>미림인이라면 여기로 다 모여라!</p>
      		<p>미림인이 다모인 여기, 같이 공부합시다!</p>
-     		<button onclick="location.href='login.html'">다모임 가입하기</button>
+     		
+
+			 <?php
+					session_start();
+					if(isset($_SESSION['user_name'])){?>
+						<button onclick="location.href='recommand.html'">모임 추천받기</button>
+						<?php
+					}else{
+					?>
+						<button onclick="location.href='login.html'">다모임 가입하기</button>
+					<?php
+					}
+					?>
      	</div>
 		 <div class="menu">
-			<a href="main.html"><img id="logo" src="img/logo.png"></a>
+			<a href="main.php"><img id="logo" src="img/logo.png"></a>
 			<a href="mypage.html"><h3 id="mypage">마이페이지</h3></a>
 			<h3>|</h3>
 			<a href="group_lookup.html"><h3 id="lookup">그룹조회</h3></a>
