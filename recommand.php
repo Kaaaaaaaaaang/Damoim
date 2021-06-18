c9f29a0ede6be65755e0a9c9818674602341766d
kne2317 <skdms2317@gmail.com>
2021-06-19 05:47:39 +0900

    일단.......백업...........

--- recommand.php
+++ recommand.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
 include "db.php";
-if(!isset($_SESSION['user_name'])){
-    echo("<script>location.href='login.html';</script>"); 
-}
+if(!isset($_SESSION['user_name'])) echo("<script>location.href='login.html';</script>"); 
+
 
 $q_count = $_GET['q_count'];
 $sql = "SELECT * FROM reccommand WHERE num =".$q_count;
@@ -19,23 +18,48 @@ $row = mysqli_fetch_array($result);
   <link rel='icon' type='images/png'href='img/logo.png'>
 	<link href="css/recommand.css" rel="stylesheet" type="text/css" />
   <link href="css/common.css" rel="stylesheet" type="text/css" />
+
+  <script>
+    function getAnswer() {
+      const answerlist= document.getElementsByName('answer');
+  
+      answerlist.forEach((node) => {
+        if(node.checked)  {
+          var result=node.value;
+          if(result == "1"){<?php if('<script>doucument.write(result)</script>'==1)$case="1";?> result=1;}
+          else if(result == "2"){<?php if('<script>doucument.write(result)</script>'==2)$case="2";?>result=2;}
+          else if(result == "3"){<?php if('<script>doucument.write(result)</script>'==3)$case="3";?>result=3;}
+        }
+      }) 
+
+      <?php 
+          echo $case;
+          if($row['num']==1)setcookie('q1',$case, time() + 1800, '/');
+          else if($row['num']==2)setcookie('q2',$case, time() + 1800, '/');
+          else if($row['num']==3)setcookie('q3',$case, time() + 1800, '/');
+          else if($row['num']==4)setcookie('q4',$case, time() + 1800, '/');
+          else if($row['num']==5)setcookie('q5',$case, time() + 1800, '/');
+          else if($row['num']==6)setcookie('q6',$case, time() + 1800, '/');
+          ?>
+    }
+  </script>
 </head>
 <body style="overflow:hidden;">
 	<div class="container">
      	<div class="background_box"></div>
      	<div class="container2">
      		<h3 id="question"><?php echo $row['Q']?></h3>
-               <hr>
+        <hr>
      		<div class="radio-wrap">
-                 <input type="radio" name="answer" id="one"/>
+                 <input type="radio" name="answer" id="one" value="1"  checked/>
                  <label for="one"><?php echo $row['A1']?></label>
                </div>
                <div class="radio-wrap">
-                 <input type="radio" name="answer" id="two"/>
+                 <input type="radio" name="answer" id="two"value="2"/>
                  <label for="two"><?php echo $row['A2']?></label>
                </div>
                <div class="radio-wrap">
-                 <input type="radio" name="answer" id="three"/>
+                 <input type="radio" name="answer" id="three"value="3"/>
                  <label for="three"><?php echo $row['A3']?></label>
                </div>
                <div class="button_box">
@@ -44,13 +68,13 @@ $row = mysqli_fetch_array($result);
                     $data = mysqli_query($conn, $query);
                     if(mysqli_num_rows($data)==$q_count){
                     ?>
-                    <button onclick="location.href='recommand_result.php'">result &nbsp; →</button>
+                    <button onclick="getAnswer();location.href='recommand_result.php'">result &nbsp; →</button>
                       <?php
                     }else{
-                      ?><button onclick="location.href='recommand.php?q_count=<?php echo ($row['num']+1);?>'">Next &nbsp; →</button><?php
+                      ?>
+                      <button onclick="getAnswer();location.href='recommand.php?q_count=<?php echo ($row['num']+1);?>'">Next &nbsp; →</button><?php
                     }                    
                  ?>
-     		          
                </div>
      	</div>
        <div class="menu">
