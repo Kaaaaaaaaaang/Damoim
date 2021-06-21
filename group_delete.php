202aefcdfe857d0cf5fdbaba39f860a6a2bc72f0
sori kang <s2019s01@e-mirim.hs.kr>
2021-06-22 03:09:25 +0900

    그룹 삭제

--- group_delete.php
+++ group_delete.php
@@ -1,1 +1,31 @@
-<!-- 그룹 삭제 -->
+<?php
+
+include "db.php";
+session_start();
+
+$user_name = $_SESSION['user_name'];
+$name = $_POST['name'];
+
+$sql = "DELETE FROM study WHERE title = '$name' and leader = 'user_name';";
+
+$result = mysqli_query($conn, $sql);
+
+if($result==true) {
+?>
+  <script>
+      alert("그룹 삭제가 완료되었습니다.");
+      location.href='mygroup.php';
+  </script>
+
+<?php
+}else{
+?>
+
+<script>
+    alert("그룹 삭제에 실패했습니다.");
+    location.href='mygroup.php';
+</script>
+
+<?php
+}
+?>
