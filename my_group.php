73b0fabd41667e8e5dc224d0d4593351e8bd23c3
sori kang <s2019s01@e-mirim.hs.kr>
2021-06-22 23:27:39 +0900

    마이그룹 페이지

--- my_group.php
+++ my_group.php
@@ -8,35 +8,16 @@
 <link href="css/common.css" rel="stylesheet" type="text/css" />
 </head>
 <body style="overflow-x:hidden;">
-  <div class="container">
+<div class="container">
     <div class="container2">
-      <div class="group_header_box">
-        <h3 id="hashtag"></h3>
-        <h3 id="title" style="float: left;">수빈이와 함께하는 안드로이드 교실</h3>
-        <button type="button" id="group_edit_btn" onclick="location.href='group_edit.html'"><img src="img/pen.png" alt=""> </button>
-      </div>
+      <h3 id="hashtag"></h3>
+      <h3 id="title" style="float: left;"><?php echo $row['title'];?></h3><br>
       <hr>
       <div class="about_box">
         <h2>스터디 소개</h2>
         <div class="ab">
           <p id="group_about">
-            스터디 소개깊은 대화까지도 자연스러워지는 Topics for Racers<br>
-            - 사랑&관계, 여행, 가족, 뮤지션, 가치관 얘기부터 비즈니스영어까지.<br>
-            - 알고나면 활용도100% 리얼한 대화표현들!<br>
-            #망쳤어 #대박이야 #완전다운됐어 #일석이조지! #뒤통수맞았어 #운명적이야! #이제 말이 좀 통하네!<br>
-            <br>
-            Time table<br>
-            1. How was your week? 한 주 공유하기<br>
-            지난 주를 어떻게 보냈는지 얘기 나눠요.<br>
-            <br>
-            2. Dialogue of the day 샘플 대화 연습하기<br>
-            예시로 주어진 대화를 그대로 말해보며 새로운 느낌의 표현들을 익혀봐요.<br>
-            <br>
-            3. Idioms&Practice 대화 속 idioms 이해하기<br>
-            대화에 나온 idiom, 정확히 어떤 뉘앙스일까?<br>
-            다같이 배워보고 다른 상황에도 적용해봐요.<br>
-            <br>
-            영어가 당신의 즐거운 일상이 될 거에요 :)
+            <?php echo $row['intro'];?>
           </p>
         </div>
       </div>
@@ -44,21 +25,14 @@
       <div class="head_about_box">
         <div class="box">
           <h2>모임장 소개</h2><br>
-          <img id="head_profile_img"  src="img/sample_proflie.png"><br>
-          <span id="head_name">다모임</span><br>
-          <span id="head_hakgwa">소프트웨어과</span><br>
-          <span id="head_age">3학년</span>
+          <img id="head_profile_img" src="<?php echo $row['img_path']; ?>"><br>
+          <span id="head_name"><?php echo $row2['name'];?></span><br>
+          <span id="head_hakgwa"><?php echo $row2['major'];?></span><br>
+          <span id="head_age"><?php echo $row2['grade'];?></span>
         </div>
         <div class="ab">
           <p id="head_about">
-            <b>Welcome you all!❤ Let's have fun!</b><br>
-            <br>
-            ✔︎ Helena 리더의 상세이력<br>
-            · LST 후기 평점 ★5.0/5.0 (7)<br>
-            · 해외 5년 거주경험<br>
-            · (전) 영문학과 졸업<br>
-            · (전) 중~고등생 1:1과외/영어학원 티칭경험<br>
-            · (전) 그룹스터디 Shadowing 영어티칭 경험<br>
+            <? echo $row2['intro'];?>
           </p>
         </div>
       </div>
@@ -66,13 +40,13 @@
       <div class="information_box">
       <h2>상세한 정보</h2>
       <div class="ab">
-      <span>학년</span> <span id="age">3학년</span> | 
-      <span>인원</span> <span id="people">8</span><span>명</span><br>
+      <span>학년</span> <span id="age"><?php echo $row['grade'];?></span> | 
+      <span>인원</span> <span id="people"><?php echo $row['max_mem'];?></span><span>명</span><br>
       <span>날짜</span> <span id="day">주말</span> |
-      <span>시간</span> <span id="time">4시 - 8시</span>
+      <span>시간</span> <span id="time"><?php echo $row['start_time'];?>시 - <?php echo $row['end_time'];?>시</span>
       </div>
       </div>
-      <button id="group_out_btn" onclick="location.href='login.html'">모임 탈퇴하기</button><br><br>
+      <button id="group_apply_btn" onclick="location.href='group_out.php'">모임 탈퇴하기</button><br><br>
     </div>
   </div>
     <div class="menu">
