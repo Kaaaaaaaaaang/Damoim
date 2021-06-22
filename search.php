2568cd59196f54b4c9bd98687ed3bce6a830b989
sori kang <s2019s01@e-mirim.hs.kr>
2021-06-23 01:05:03 +0900

    in 조건문

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ $major = $_POST['major'];
 $grade = $_POST['grade'];
 $category = $_POST['category'];
 
-$sql = "where major='$major' and grade='$grade' and category='$category'";
+$sql = "where '$major' in ('뉴미디어소프트웨어과', '뉴미디어웹솔루션과', '뉴미디어디자인과') and '$grade' in ('1학년', '2학년', '3학년') and category='$category';";
 ?>
 <script>
     location.href="group_lookup.php?search=<?php echo $sql;?>";
